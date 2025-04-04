7320933e0696f3811d738b95e052f90dc8a97dee
Elkrahop17 <di.jimeneze@duocuc.cl>
2025-04-04 11:20:22 -0300

    Arreglo en Subir productos, para que se vean en la pagina

--- petamigos/settings.py
+++ petamigos/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = 'petamigos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'productos_animales',
+        'USER': 'root',
+        'PASSWORD': '',  # o la contraseña que tengas
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
